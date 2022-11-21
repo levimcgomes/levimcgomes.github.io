3215f95e95196d34e543f99f96fcc92111c73093
Leviathan <levi.mcgomes@gmail.com>
2022-11-21 19:07:13 +0000

    Amended CDN URL

--- app/next.config.js
+++ app/next.config.js
@@ -2,5 +2,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
     // Use the CDN in production and localhost for development.
-    assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
+    assetPrefix: isProd ? 'https://cdn.statically.io/gh/levi-gomes/levi-gomes.github.io/gh-pages/' : '',
 }
