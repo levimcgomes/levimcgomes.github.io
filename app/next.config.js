a13a95918e622b948b573a49bfb76e4921fb2bd1
Leviathan <levi.mcgomes@gmail.com>
2022-11-21 19:04:12 +0000

    Added CDN for static file serving, as GH pages seems to ignore _next folder

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = nextConfig
+module.exports = {
+    // Use the CDN in production and localhost for development.
+    assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
+}
