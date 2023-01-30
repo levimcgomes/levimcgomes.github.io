3b1083ced001d6e48f2e92123c34b8b8b9e54a7d
Leviathan <levi.mcgomes@gmail.com>
2023-01-30 19:06:43 +0000

    Solved rehydration issue on index.jsx

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -5,6 +5,7 @@ import matter from 'gray-matter'
 import Head from 'next/head'
 import Link from 'next/link'
 import Post from '../components/Post'
+import ClientOnly from '../components/ClientOnly'
 import {sortByDate} from '../utils'
 
 export default function Home({ posts }) {
@@ -25,18 +26,20 @@ export default function Home({ posts }) {
             <p className='text'>
                 So, what&#39;s this all <em>about</em>? It&#39;s about how I made a game. Sometimes. Really, it&#39;s about whatever I feel like writing. Most of the time, it will be about making games, but everything is fair game for me. I&#39;ll try to post every other week, but due to other responsiblities (aka. school), I might be unable to fullfil a schedule. If you&#39;d like to contact me for some reason (preferably related to this blog), you can find me on <Link href='https://discordapp.com/users/924653755763396639' passHref>Discord</Link>. However, please don&#39;t send a friend request. I won&#39;t accept it.
             </p>
-            <h2>Random stuff</h2>
-            <p className='text'>
-                This paragraph is just what it&#39;s name says it is: random stuff. Firstly, some links.
-                <ul>
-                    <li><Link href='https://github.com/levimcgomes/' passHref>My GitHub account</Link></li>
-                    <li><Link href='https://github.com/levimcgomes/levimcgomes.github.io' passHref>This website's source code</Link></li>
-                </ul>
-                Next, some utilities I&#39;ve made for myself and put in this site for ease of access. Enjoy! (please note that these were made for personal use, so they&#39;re very low quality tools; however, I do think they&#39;re useful)
-                <ul>
-                    <li><Link href='/hidden/color' passHref>Color Palette Helper</Link></li>
-                </ul>
-            </p>
+            <ClientOnly>
+                <h2>Random stuff</h2>
+                <p className='text'>
+                    This paragraph is just what it&#39;s name says it is: random stuff. Firstly, some links.
+                    <ul>
+                        <li><Link href='https://github.com/levimcgomes/' passHref>My GitHub account</Link></li>
+                        <li><Link href='https://github.com/levimcgomes/levimcgomes.github.io' passHref>This website&#39;s source code</Link></li>
+                    </ul>
+                    Next, some utilities I&#39;ve made for myself and put in this site for ease of access. Enjoy! (please note that these were made for personal use, so they&#39;re very low quality tools; however, I do think they&#39;re useful)
+                    <ul>
+                        <li><Link href='/hidden/color' passHref>Color Palette Helper</Link></li>
+                    </ul>
+                </p>
+            </ClientOnly>
         </div>
     )
 }
