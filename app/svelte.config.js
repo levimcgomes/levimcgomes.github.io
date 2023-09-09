06545a6778e42e9fc434e64eecbd537dc30823a4
Leviathan <levi.mcgomes@gmail.com>
2023-09-09 11:38:21 +0100

    Created Svelte project

---
+++ app/svelte.config.js
@@ -0,0 +1,18 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const dev = process.argv.includes('dev');
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH
+		}
+	}
+};
+
+export default config;
