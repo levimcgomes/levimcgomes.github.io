8f97d8a611b7b752b4f4c9b843b04813b6787186
Leviathan <levi.mcgomes@gmail.com>
2023-09-09 18:08:20 +0100

    Added simple error page

---
+++ app/src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<h1>{$page.status}: {$page.error?.message}</h1>
+<div>
+	<h3>Here's some debug info:</h3>
+	{$page.error?.path}
+	{$page.error?.pwd}
+</div>
