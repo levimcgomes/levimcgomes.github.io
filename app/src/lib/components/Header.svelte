b138f94d14a24b176c1a10a46f0c530b68cee33a
Leviathan <levi.mcgomes@gmail.com>
2023-09-13 17:19:24 +0100

    Made the header responsive

--- app/src/lib/components/Header.svelte
+++ app/src/lib/components/Header.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <nav
-	class="bg-grey-100 text-grey-900 flex justify-between content-center py-6 px-4 md:sticky top-0 z-10 [&>*]:h-min"
+	class="bg-grey-100 text-grey-900 flex flex-col items-center justify-between md:flex-row md:justify-between md:content-center py-6 px-4 md:sticky top-0 z-10 [&>*]:h-min"
 >
 	<a class="no-underline hover:no-underline transition-none" href="/"
 		><h2 class="no-underline text-grey-900 not-italic">How I Made A Game</h2></a
