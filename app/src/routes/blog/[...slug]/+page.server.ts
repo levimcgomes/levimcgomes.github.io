d09136be45ed67ab5ef32832c4b43ce43de7d4d1
Leviathan <levi.mcgomes@gmail.com>
2023-09-14 10:26:50 +0100

    Added a redirect to support legacy links (as they were for the previous blog)

--- app/src/routes/blog/[...slug]/+page.server.ts
+++ app/src/routes/blog/[...slug]/+page.server.ts
@@ -1,6 +1,7 @@
 import { postFromPath } from '$lib/server/post';
 import type { Post } from '$lib/server/post.type';
-import { error } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
+import fs from 'fs';
 import type { PageServerLoad } from './$types';
 // File interfacing
 import markdown from '$lib/server/markdown';
@@ -13,6 +14,13 @@ export const load = (({ params }) => {
 		post = postFromPath(path, true);
 		html = markdown(post.content);
 	} catch (e) {
+		// For backwards compatibility
+		// The previous blog separated paths in the blog route
+		// by _ (cause next doesn't support variadic dynamic routes, w Svelte)
+		// so we do a simple redirect to ensure legacy links still work
+		if (fs.existsSync(path.replaceAll('_', '/'))) {
+			throw redirect(301, '/blog/' + params.slug.replaceAll('_', '/'));
+		}
 		throw error(404, {
 			message: 'Post not found!',
 			path: path,
