874847afea1904bacceec9e4b6d3ab003f2e9fc5
Leviathan <levi.mcgomes@gmail.com>
2023-09-17 11:35:11 +0100

    Added post list caching

--- app/src/lib/server/post.ts
+++ app/src/lib/server/post.ts
@@ -6,19 +6,30 @@ import glob from 'glob';
 // Markdown parsing
 import matter from 'gray-matter';
 
+// Use caching (or memoization if you're fancy),
+// this way we can call this multiple times without
+// a big performance cost
+let cachedPostList: Post[] | undefined = undefined;
+
 export function getPostList(limit = -1): Post[] {
-	// All posts should be in /src/posts/,
-	// either in the top - level or inside
-	// sub folders
-	const postPaths = glob.sync('./src/posts/**/*.md');
+	if (!cachedPostList) {
+		// Just a sanity check to make sure caching is working properly
+		console.log('COMPUTE POST LIST');
+		// All posts should be in /src/posts/,
+		// either in the top - level or inside
+		// sub folders
+		const postPaths = glob.sync('./src/posts/**/*.md');
 
-	const posts = postPaths.map((path) => postFromPath(path));
+		const posts = postPaths.map((path) => postFromPath(path));
 
-	if (limit === -1) return sortPostsByDate(posts);
-	else return sortPostsByDate(posts).slice(0, limit);
+		cachedPostList = sortPostsByDate(posts);
+	}
+
+	if (limit === -1) return cachedPostList;
+	else return cachedPostList.slice(0, limit);
 }
 
-export function postFromPath(path: string, getContent = false) {
+export function postFromPath(path: string) {
 	const file = fs.readFileSync(path);
 
 	const parsed = matter(file);
@@ -26,31 +37,17 @@ export function postFromPath(path: string, getContent = false) {
 	// bout ours is European style, so we need to do some juggling
 	const splitDate = (parsed.data['date'] as string).split('/');
 	const date = new Date(splitDate[2] + '/' + splitDate[1] + '/' + splitDate[0]);
-
-	if (getContent) {
-		return {
-			path: path,
-			title: parsed.data['title'] as string,
-			date: date,
-			date_string: parsed.data['date'] as string,
-			cover_image: parsed.data['cover_image'] as string,
-			cover_image_exists: fs.existsSync(process.cwd() + '/static/' + parsed.data['cover_image']),
-			tags: parsed.data['tags'] as string[],
-			excerpt: parsed.data['excerpt'] as string,
-			content: parsed.content
-		} as Post;
-	} else {
-		return {
-			path: path,
-			title: parsed.data['title'] as string,
-			date: date,
-			date_string: parsed.data['date'] as string,
-			cover_image: parsed.data['cover_image'] as string,
-			cover_image_exists: fs.existsSync(process.cwd() + '/static/' + parsed.data['cover_image']),
-			tags: parsed.data['tags'] as string[],
-			excerpt: parsed.data['excerpt'] as string
-		} as Post;
-	}
+	return {
+		path: path,
+		title: parsed.data['title'] as string,
+		date: date,
+		date_string: parsed.data['date'] as string,
+		cover_image: parsed.data['cover_image'] as string,
+		cover_image_exists: fs.existsSync(process.cwd() + '/static/' + parsed.data['cover_image']),
+		tags: parsed.data['tags'] as string[],
+		excerpt: parsed.data['excerpt'] as string,
+		content: parsed.content
+	} as Post;
 }
 
 function sortPostsByDate(posts: Post[]): Post[] {
